a68f3141a90c4edc8e0406518e88fd3b57bf1dbf
amankr1635 <amankr.1635@gmail.com>
2022-12-08 17:42:42 +0530

    editted 2

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,14 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
-    res.send('any dummy text')
+    res.send('My first api!')
 });
-
 
 router.get('/test-you', function(req, res){
     console.log("I am here")
-    res.send("very important text")
-})
-
-
+    res.send("My Name Is Aman Kumar")
+});
 module.exports = router;
